b05faa2dc5a7fb6c45058cbd354958e84ee5790b
Jo Dickson <jogreybill@gmail.com>
2020-03-19 12:00:52 -0400

    Initial commit

---
+++ safety-utilities.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Safety Utilities
+Description: Utilities for UCF's Safety website.
+Version: 0.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/Safety-Utilities
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
